f0e636a1311892ead58b44d73e5cd824e954eb21
Nakusu <nakusu35@hotmail.fr>
2020-11-26 16:00:32 +0100

    HeaderRequest

---
+++ srcs/HeaderRequest.hpp
@@ -0,0 +1,33 @@
+#ifndef HEADERREQUEST_HPP
+#define HEADERREQUEST_HPP
+
+#include <iostream>
+#include <map>
+#include <string>
+
+class HeaderRequest {
+	public:
+		HeaderRequest() {
+			this->_size = 0;
+		}
+		virtual	~HeaderRequest() {
+			return ;
+		}
+		void											add_content(std::string key, std::string content) {
+			this->_content.insert(std::pair<std::string, std::string>(key, content));
+		}
+		size_t											get_size() const {
+			return (this->_size);
+		}
+		std::map<std::string, std::string>				get_content() const {
+			return (this->_content);
+		}
+		std::string										get_content(std::string key) {
+			return (this->_content[key]);
+		}
+	private:
+		std::map<std::string, std::string>				_content;
+		size_t											_size;
+};
+
+#endif
