3646a2b3e826687b88b8e953c09dcd758ef7326f
Nakusu <nakusu35@hotmail.fr>
2020-12-26 19:21:20 +0100

    DoPut

--- srcs/webserv.cpp
+++ srcs/webserv.cpp
@@ -26,13 +26,12 @@ void		Exec(ServerWeb *serv, Client *client, int i, char **env){
 	HeaderRequest *header = new HeaderRequest();
 	Execution exec = Execution(serv, serv->getVS(i), req, header, env);
 	std::string Method = req->get_method();
-
 	if (Method == "POST")
 		req->getDatas();
 	if (!exec.checkMethod())
 		exec.searchError405();
 	if (!exec.needRedirection() && exec.checkMethod()){
-		if (!exec.searchIndex() && !exec.initCGI() && !exec.binaryFile())
+		if (!exec.doPut() && !exec.searchIndex() && !exec.initCGI() && !exec.binaryFile())
 			exec.searchError404();	
 	}
 	delete header;
